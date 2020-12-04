8773c1009dbfe3dee4364b2e212f65b301e8430f
rsrochab <rs.rochab@gmail.com>
2020-12-03 21:21:11 -0300

    Ajustes
    Ajustes

--- Exercicio03.java
+++ Exercicio03.java
@@ -5,7 +5,7 @@ public class Exercicio03 {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		Livro l = new Livro();
+		Livro l = new Livro("Os contos de Java", "Rodrigo Rocha", 23);
 		l.Alterar(scan);
 	}
 }
